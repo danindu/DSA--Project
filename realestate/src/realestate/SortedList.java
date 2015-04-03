fa0ca6f2fa3581dadfa7a45fff494987664cc3e6
nc32 <nadeeshac32@gmail.com>
2015-04-03 22:00:02 +0530

    implemented SortedList insert method

--- realestate/src/realestate/SortedList.java
+++ realestate/src/realestate/SortedList.java
@@ -6,8 +6,38 @@ package realestate;
 
 /**
  *
- * @author Danindu
+ * @author team Olympians
  */
-public class SortedList {
+public class SortedList extends List{
+
+    public SortedList(int maxItems) {
+        super(maxItems);
+    }
     
+    public SortedList() {
+        super(50);
+    }
+    
+    @Override
+    public void insert(Listable item) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int insertIndex     = 0;
+        boolean searchMore  = ( insertIndex < numOfItems );
+        
+        while(searchMore){
+            int compareResult = item.compareTo(list[insertIndex]);
+            if( compareResult < 0 ){
+                searchMore  = false;
+            }
+            else{
+                insertIndex++;
+                searchMore  = ( insertIndex < numOfItems );
+            }
+        }
+        for(int index = numOfItems ; index > insertIndex ; index-- ){
+            list[index]     = list[index-1];
+        }
+        list[insertIndex]   = item.copy();
+        numOfItems++;
+    }    
 }
