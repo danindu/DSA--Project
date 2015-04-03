b6bbc55c51712b2c384eec17358ac9150d9d7ab8
Koshila <koshibasnagala@gmail.com>
2015-04-03 22:09:58 +0530

    implemented SortedList class

--- realestate/src/realestate/SortedList.java
+++ realestate/src/realestate/SortedList.java
@@ -40,4 +40,49 @@ public class SortedList extends List{
         list[insertIndex]   = item.copy();
         numOfItems++;
     }    
+    
+    @Override
+    public boolean isThere(Listable item) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int compareResult;
+        int midPointIndex;
+        int firstIndex      = 0;
+        int lastIndex       = numOfItems - 1;
+        boolean searchMore  = ( firstIndex <= lastIndex );
+        boolean found       = false;
+        
+        while( searchMore && !found ){
+            midPointIndex   = ( firstIndex + lastIndex )/2;
+            compareResult   = item.compareTo(list[midPointIndex]);   
+            if(compareResult == 0)
+                found = true;
+            else if(compareResult > 0){
+                firstIndex  = midPointIndex + 1;
+                searchMore  = ( firstIndex <= lastIndex );
+            }
+            else{
+                lastIndex   = midPointIndex -1;
+                searchMore  = ( firstIndex <= lastIndex );
+            }
+        }
+        return found;
+    }
+    
+    @Override
+    public void delete(Listable item) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int deleteIndex = 0;
+        
+        while( item.compareTo(list[deleteIndex]) != 0 ){
+            deleteIndex++;
+        }
+        for(int index = deleteIndex+1 ; index < numOfItems ; index++ ){
+            list[index-1] = list[index];
+        }
+        numOfItems--;
+        if(currentPosition == 0)
+            currentPosition = numOfItems-1;
+        else
+            currentPosition--;
+    }
 }
