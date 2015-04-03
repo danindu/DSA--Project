0189c44b39b5846266906ba08ed49d3d5ab1ee39
danindu <danindu.sg@gmail.com>
2015-04-03 21:51:53 +0530

    add List ADT abstract class

--- realestate/src/realestate/List.java
+++ realestate/src/realestate/List.java
@@ -6,8 +6,45 @@ package realestate;
 
 /**
  *
- * @author Danindu
+ *  @author team Olympians
  */
-public class List {
+public abstract class List {
+    protected Listable[] list;
+    protected int numOfItems;
+    protected int currentPosition;
     
+    public List(int maxItems){
+        numOfItems = 0;
+        list = new Listable[maxItems];
+    }
+    
+    public boolean isFull(){
+        return (list.length == numOfItems);
+    }
+    
+    public int getLength(){
+        return numOfItems;
+    }
+    
+    public abstract boolean isThere(Listable item);
+    
+    public abstract Listable retieve(Listable item);
+    
+    public abstract void insert(Listable item);
+    
+    public abstract void delete(Listable item);
+    
+    public void resetlist(){
+        currentPosition = 0;
+    }
+    
+    public Listable getNextItem(){
+        Listable nextItem = list[currentPosition];
+        if( currentPosition == (numOfItems-1) )
+            currentPosition = 0;
+        else
+            currentPosition++;
+
+        return nextItem.copy();
+    }    
 }
