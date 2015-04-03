e647116ff650d21e2ef8e184cd422c22b6712dac
nc32 <nadeeshac32@gmail.com>
2015-04-03 21:24:26 +0530

    implemented ListHouse abstract methods

--- realestate/src/realestate/HouseFile.java
+++ realestate/src/realestate/HouseFile.java
@@ -4,10 +4,36 @@
  */
 package realestate;
 
+import java.io.IOException;
+
 /**
  *
  * @author Danindu
  */
 public class HouseFile {
+    public static boolean searchMoreHouses(){
+        if(!inputFileOpen || newHouseFirstLine == null){ return false; }
+        else{ return true; }
+    }
     
+    public static ListHouse getNextHouse() throws IOException{
+        int lotNumber       = 0;
+        String firstName    = "abcd";
+        String lastName     = "abcd";
+        int price           = 0;
+        int squareFeet      = 0;
+        int numOfBedrooms   = 0;
+        
+        lotNumber       = Integer.parseInt(newHouseFirstLine);
+        firstName       = inputFile.readLine();
+        lastName        = inputFile.readLine();
+        price           = Integer.parseInt(inputFile.readLine());
+        squareFeet      = Integer.parseInt(inputFile.readLine());
+        numOfBedrooms   = Integer.parseInt(inputFile.readLine());
+        
+        newHouseFirstLine = inputFile.readLine();
+        
+        ListHouse house = new ListHouse(lotNumber,firstName,lastName,price,squareFeet,numOfBedrooms);
+        return house;
+    }
 }
--- realestate/src/realestate/ListHouse.java
+++ realestate/src/realestate/ListHouse.java
@@ -17,14 +17,28 @@ public class ListHouse implements Listable{
     private int squareFeet;
     private int numOfBedrooms;
     
-    @Override
-    public int compareTo(Listable other) {
+    ListHouse(int lotNumber, String firstName, String lastName, int price, int squareFeet, int numOfBedrooms) {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.lotNumber      = lotNumber;
+        this.firstName      = firstName;
+        this.lastName       = lastName;
+        this.price          = price;
+        this.squareFeet     = squareFeet;
+        this.numOfBedrooms  = numOfBedrooms;
+    }
+        
+    @Override
+    public int compareTo(Listable otherHouse) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ListHouse other = (ListHouse)otherHouse;
+        return (this.lotNumber - other.lotNumber);
     }
 
     @Override
     public Listable copy() {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ListHouse newcopy = new ListHouse(this.lotNumber, this.firstName, this.lastName, this.price, this.squareFeet, this.numOfBedrooms);
+        return newcopy;
     }
     
     public int lotNumber(){
